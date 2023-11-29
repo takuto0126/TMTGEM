54a8a7d01b3d51212d3b08543ec2bc1070e59700
takuto0126 <takuto.minami126@gmail.com>
2023-11-29 09:27:18 +0900

    Tohoku/em/tohoku.ctl -> tmtgem.ctl

--- Tohoku/em/run.sh
+++ Tohoku/em/run.sh
@@ -1,1 +1,19 @@
-../../tmtgem/src_post/run.sh
+# run script for Macbook Pro 15 inch
+#!/bin/sh
+
+# intel oneapi environment
+source /opt/intel/oneapi/setvars.sh
+
+# openMP threads
+export OMP_NUM_THREADS=8
+
+src=../../tmtgem/solver
+cd $src
+make clean
+make
+cd -
+
+rm bxyz/* exyz/* vxyz/*
+
+time ${src}/ebfem_tsunamiEM.exe < tmtgem.ctl
+
