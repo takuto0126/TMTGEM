5db6bd06ed132b250f477ec58cc7f5826678e909
takuto0126 <takuto.minami126@gmail.com>
2023-11-29 11:42:38 +0900

    Mediterranean/em/run.sh bxyzplot.sh become symbolic

--- Mediterranean/em/run.sh
+++ Mediterranean/em/run.sh
@@ -1,18 +1,1 @@
-# run script for Macbook Pro 15 inch
-#!/bin/sh
-source /opt/intel/bin/compilervars.sh intel64
-source /opt/intel/bin/debuggervars.sh intel64
-
-OMP_NUM_THREADS=4
-
-
-src=../../tmtgem/solver
-cd $src
-make clean
-make
-cd -
-
-rm bxyz/* exyz/* vxyz/*
-
-time ${src}/ebfem_tsunamiEM.exe < mediterranean.ctl
-
+../../tmtgem/src_post/run.sh
