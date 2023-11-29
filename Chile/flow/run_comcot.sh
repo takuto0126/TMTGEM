d7e15393f5120a3001a04eb2c2cb8c386c42db90
takuto0126 <takuto.minami126@gmail.com>
2023-11-29 16:39:12 +0900

    Chile/flow/*.sh becomes symbolic

--- Chile/flow/run_comcot.sh
+++ Chile/flow/run_comcot.sh
@@ -1,14 +1,1 @@
-# coded on 2018.11.13
-#!/bin/bash
-
-ctl=comcot.ctl
-src="../../comcotv1_7"
-
-cd $src
-make clean
-make
-cd -
-
-time ${src}/comcot.exe < ${ctl}
-
-
+../../tmtgem/src_post/run_comcot.sh
