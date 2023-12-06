4542a1ee59544027bb18ce11c2e8fa0a9c9bcb92
takuto0126 <takuto.minami126@gmail.com>
2023-12-06 17:52:12 +0900

    tests/plot_tohoku_em.sh range is changed

--- tests/plot_tohoku_em.sh
+++ tests/plot_tohoku_em.sh
@@ -5,7 +5,7 @@ fil2=Tohoku_ref/b14_bxyz_ts.dat
 
 gmt begin tohoku_em pdf
 
-gmt basemap -JX20/10 -R0/5/-5/5 -Bxa1 -Bya1 -BWeSn
+gmt basemap -JX20/10 -R0/5/-1/1 -Bxa1 -Bya1 -BWeSn
 awk '{print($1 /60, $2)}' $fil1 | gmt plot -W1,blue
 awk '{print($1 /60, $2)}' $fil2 | gmt plot -W1,red
 
